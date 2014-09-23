3d7f7cea33965a76b6e987dd2409424f0530e4db
joebartos <CL@christines-mbp.home>
2014-09-23 15:48:38 -0400

    Traffic Light Homework
    Finished
    I just learned that a couple of blank lines at the end of your
    Javascript code will disable portions of it. A 1/2 hour lesson.

--- GA_project_2_traffic/js/application.js
+++ GA_project_2_traffic/js/application.js
@@ -1,1 +1,13 @@
-alert("hello world")
+function buttonClick(lightOn){
+  var yellowLight = document.getElementById('yellow');
+  var redLight = document.getElementById('red');
+  var greenLight = document.getElementById('green');
+  
+  redLight.style.backgroundColor = "black";
+  greenLight.style.backgroundColor = "black";
+  yellowLight.style.backgroundColor = "black";
+  
+  var light = document.getElementById(lightOn);
+  
+  light.style.backgroundColor = lightOn;
+}
