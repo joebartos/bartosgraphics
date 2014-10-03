3ac44695b2e15843d15de81eb88ad64d7e53e469
joebartos <CL@Christines-MacBook-Pro.local>
2014-10-02 20:58:09 -0400

    Final homework project 3

--- GA_project_3_gallery/js/application.js
+++ GA_project_3_gallery/js/application.js
@@ -1,28 +1,29 @@
-var images = ["jbWeb14_KbookBig1.jpg, jbWeb14_KbookBig2.jpg, jbWeb14_KbookBig3.jpg, 
-jbWeb14_KbookBig4.jpg, jbWeb14_KbookBig5.jpg, jbWeb14_KbookBig6.jpg"]
-var currentImage = 0
+var images = ["img/jbWeb14_KbookBig1.jpg", "img/jbWeb14_KbookBig2.jpg", "img/jbWeb14_KbookBig3.jpg", 
+"img/jbWeb14_KbookBig4.jpg", "img/jbWeb14_KbookBig5.jpg", "img/jbWeb14_KbookBig6.jpg"];
+var currentImage = 0;
 
 function changePic(whichNum){
-var img = document.getElementById('mainImage');
-img.setAttribute('src', images[whichNum]);
-currentImage = whichNum;
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[whichNum]);
+	currentImage = whichNum;
 }
-	/*call method * alert("clicked");*/
+/*call method * alert("clicked");*/
+
 
 function nextImage() {
-currentImage++;
-if(currentImage>images.length){
-	currentImage=0;
-}
-var img = document.getElementById('mainImage');
- img.setAttribute('src', images[currentImage]);
+	currentImage++;
+	if(currentImage>images.length){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
 }
 
 function prevImage() {
 	currentImage=currentImage-1;
-if(currentImage===images.length){
-	currentImage=0;
-}
-var img = document.getElementById('mainImage');
-img.setAttribute('src', images[currentImage]);
+	if(currentImage===-1){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
 }
