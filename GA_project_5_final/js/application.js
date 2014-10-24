bdb6a8ee8ce3605f4917268d99bfaeec2265b3f3
joebartos <CL@christines-mbp.home>
2014-10-24 17:25:21 -0400

    Finalized navigation, brute force style

--- GA_project_5_final/js/application.js
+++ GA_project_5_final/js/application.js
@@ -1,29 +1,59 @@
-var images = ["img/galleryBig1.jpg", "img/galleryBig2.jpg", "img/galleryBig3.jpg", 
-"img/galleryBig4.jpg", "img/galleryBig5.jpg", "img/galleryBig6.jpg"];
+var imagesTime = ["img/TimeBig1.jpg", "img/TimeBig2.jpg", "img/TimeBig3.jpg", 
+"img/TimeBig4.jpg", "img/TimeBig5.jpg", "img/TimeBig6.jpg"];
 var currentImage = 0;
 
-function changePic(whichNum){
+function changeTime(whichNum){
 	var img = document.getElementById('mainImage');
-	img.setAttribute('src', images[whichNum]);
+	img.setAttribute('src', imagesTime[whichNum]);
 	currentImage = whichNum;
 }
+
+var imagesAfl = ["img/AFLBig1.jpg", "img/AFLBig2.jpg", "img/AFLBig3.jpg", 
+"img/AFLBig4.jpg", "img/AFLBig5.jpg", "img/AFLBig6.jpg"];
+var currentImage = 0;
+
+function changeAfl(whichNum){
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', imagesAfl[whichNum]);
+	currentImage = whichNum;
+}
+
+
 /*call method * alert("clicked");*/
 
 
-function nextImage() {
+function nextTimeImage() {
 	currentImage++;
-	if(currentImage>=images.length){
+	if(currentImage>=imagesTime.length){
 		currentImage=0;
 	}
 	var img = document.getElementById('mainImage');
-	img.setAttribute('src', images[currentImage]);
+	img.setAttribute('src', imagesTime[currentImage]);
 }
 
-function prevImage() {
+function prevTimeImage() {
 	currentImage=currentImage-1;
 	if(currentImage===-1){
 		currentImage=0;
 	}
 	var img = document.getElementById('mainImage');
-	img.setAttribute('src', images[currentImage]);
+	img.setAttribute('src', imagesTime[currentImage]);
+}
+
+function nextAflImage() {
+	currentImage++;
+	if(currentImage>=imagesAfl.length){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', imagesAfl[currentImage]);
+}
+
+function prevAflImage() {
+	currentImage=currentImage-1;
+	if(currentImage===-1){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', imagesAfl[currentImage]);
 }
