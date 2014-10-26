c721fe3345cd18761e2ee070a181f5981f948265
joebartos <CL@Christines-MacBook-Pro.local>
2014-10-26 16:16:04 -0400

    Sunday workshop notes

--- GA_project_5_final_json/js/application.js
+++ GA_project_5_final_json/js/application.js
@@ -1,62 +1,3 @@
-// var imagesTime = ["img/TimeBig1.jpg", "img/TimeBig2.jpg", "img/TimeBig3.jpg", 
-// "img/TimeBig4.jpg", "img/TimeBig5.jpg", "img/TimeBig6.jpg"];
-// var currentImage = 0;
-
-// function changeTime(whichNum){
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesTime[whichNum]);
-// 	currentImage = whichNum;
-// }
-
-// var imagesAfl = ["img/AFLBig1.jpg", "img/AFLBig2.jpg", "img/AFLBig3.jpg", 
-// "img/AFLBig4.jpg", "img/AFLBig5.jpg", "img/AFLBig6.jpg"];
-// var currentImage = 0;
-
-// function changeAfl(whichNum){
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesAfl[whichNum]);
-// 	currentImage = whichNum;
-// }
-
-
-// /*call method * alert("clicked");*/
-
-
-// function nextTimeImage() {
-// 	currentImage++;
-// 	if(currentImage>=imagesTime.length){
-// 		currentImage=0;
-// 	}
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesTime[currentImage]);
-// }
-
-// function prevTimeImage() {
-// 	currentImage=currentImage-1;
-// 	if(currentImage===-1){
-// 		currentImage=0;
-// 	}
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesTime[currentImage]);
-// }
-
-// function nextAflImage() {
-// 	currentImage++;
-// 	if(currentImage>=imagesAfl.length){
-// 		currentImage=0;
-// 	}
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesAfl[currentImage]);
-// }
-
-// function prevAflImage() {
-// 	currentImage=currentImage-1;
-// 	if(currentImage===-1){
-// 		currentImage=0;
-// 	}
-// 	var img = document.getElementById('mainImage');
-// 	img.setAttribute('src', imagesAfl[currentImage]);
-// }
 
 var clients = [
 	{ // These are objects with keys, e.g. name and their values, "Time Warner"
@@ -80,41 +21,64 @@ var clients = [
 var pageIndex = 0;
 var currentImage = 0;
 
-function changeClient(index){
-	pageIndex = index; //pageIndex is a var above/
-	currentImage = 0;
-	var clientObj = clients[index]; //clients is a var above [] refers to index #/
-	$("#clientName").html(clientObj["name"]); //.html is a jQuery method  changing html of clientName http://api.jquery.com/html/
+function changeClient(index){//changeClient function is referenced in indexGallery list of clients onClick
+	pageIndex = index; 
+	//pageIndex is a global var above, index shares it's value and is used as an arguement in changeClient/
+	currentImage = 0; 
+	//currentIndex is a global var from above/currentImage has value locally defined as 0 to reset images by onClick
+	var clientObj = clients[index]; //clients index is set by onclick, 
+	//clients is a global var/method above, [index] refers to index #, which refers to an array of different clients info/
+	//ClientObj is a local variable, whose value is now equal to the first item in an array
+	$("#clientName").html(clientObj["name"]); 
+	//get the element with the id of clientName & give it the value at the object name in the .client method
+	//.html is a jQuery method, changing html of clientName, which is a css id used in the indexGallery html./  
+	// name, is an argument whose value is defined above in the clients method--html http://api.jquery.com/
+	// clientObj is a local var defined in the previous line/
 	$(".clientInfo").html(clientObj["clientInfo"]);
-	$("#mainImage").attr("src", clientObj["bigImg"][0]); // getting first image of array bigImg
-	for (var i=0; i < clientObj["img"].length; i++) {
-		// console.log("loop running");
-		var selector = ".thumb." + i +" img"; // i is the second class in the img tag
-		// console.log(selector);
-		// console.log($(selector));
-		// console.log(clientObj["img"][i]);
-		$(selector).attr("src", clientObj["img"][i]);
+	//client info is a css class used in the indexGallery html, clientInfo is an object above with an array/
+	$("#mainImage").attr("src", clientObj["bigImg"][0]); 
+	//src is element in html (mainImage), get src element and give it the index value of the clientObj[][] array, 
+	//clientObj[][] refers to object array in .client function above
+	// above code gets first image of array bigImg, .attr is a jQuery version of .getAttribute
+	//The getAttribute() method returns the value of the attribute with the specified name.
+	for (var i=0; i < clientObj["img"].length; i++) { //if i < length of img array, add 1 to the index value
+	//for loops through a block of code a number of times, img is an object array in the clients method above, 
+	//client obj is a local variable defined as an index of the .clients method's object arrays
+		// console.log("loop running"); this checks if loop is working
+		var selector = ".thumb." + i + " img"; //get .thumb 0 elements img (image)
+		// i is the second part ofclass in the img tag, thumb is class
+		// console.log(selector); this checks if loop is working
+		// console.log($(selector)); this checks if loop is working
+		// console.log(clientObj["img"][i]); this checks if loop is working
+		$(selector).attr("src", clientObj["img"][i]);//What do the 2 clientObj arrays do???????
+		//selector is a variable defined in previous line as class thumb + i + img, 
+		//so now selector equals the value of info in the .clients img object above
 	}
 }
 
-function changeImg(whichNum){
-	var clientObj = clients[pageIndex];
-	var img = document.getElementById('mainImage');
-	img.setAttribute('src', clientObj["bigImg"][whichNum]);
-	currentImage = whichNum;
+function changeImg(whichNum){ //changeImg function is referenced in indexGallery thumbstrip onClick
+	var clientObj = clients[pageIndex];//pageIndex is a global var defined above as 0
+	var img = document.getElementById('mainImage');//mainImage is an id referencing the main image in the gallery
+	img.setAttribute('src', clientObj["bigImg"][whichNum]);//What do the 2 clientObj arrays do???????  
+	//bigImg is object defined in function .clients above, 
+	//whichNum passes value to the arguement in the funciton changeImg
+	currentImage = whichNum; //currentImg is global variable given the value of 0 above
 }
 
-function prevImage() {
-	var clientObj = clients[pageIndex];
-	currentImage=currentImage-1;
-	if(currentImage===-1){
+function prevImage() { //prevImage function is referenced in indexGallery navBottom onClick
+	var clientObj = clients[pageIndex]; //pageIndex is a global var defined above as 0
+	currentImage=currentImage-1; //currentImage is a global variable defined above as 0
+	if(currentImage===-1){//How is this loop working??????
 		currentImage=0;
 	}
 	var img = document.getElementById('mainImage');
 	img.setAttribute('src', clientObj["bigImg"][currentImage]);
+	//The getAttribute() method returns the value of the attribute with the specified name.
+	//clientObj now references the bigImg object in the .clients function  
+	//and the value of the global variable currentImg above
 }
 
-function nextImage() {
+function nextImage() { //nextImage function is referenced in indexGallery navBottom onClick
 	var clientObj = clients[pageIndex];
 	currentImage++;
 	if(currentImage>=clientObj["bigImg"].length){
